d7857b152c22da134ea319c60d567dbfaea639ff
Abhishek Santra <abhisheksantra28@gmail.com>
2023-11-07 21:52:45 +0530

    configure store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 import HomePage from "./Pages/HomePage.jsx";
 import LoginPage from "./Pages/LoginPage.jsx";
 import SignUpPage from "./Pages/SignUpPage.jsx";
+
 
 const router = createBrowserRouter([
   {
@@ -15,23 +18,24 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <HomePage/>,
+        element: <HomePage />,
       },
       {
         path: "/signup",
-        element: <SignUpPage/>,
+        element: <SignUpPage />,
       },
       {
         path: "/signin",
-        element: <LoginPage/>,
+        element: <LoginPage />,
       },
-   
     ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
